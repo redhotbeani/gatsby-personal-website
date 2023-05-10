ed94886c24b8c6548d465b0a830dd1abaee26088
Arie de Bonth <a.debonth@gynzy.com>
2023-05-10 16:46:01 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `Gatsby personal website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
